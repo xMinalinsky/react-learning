0672ae98f1cd1a9b252552aadc4a00f28a327e81
xMinalinsky <trialish77@gmail.com>
2022-07-15 01:17:37 +0800

    3124 reqwteqt

--- test/src/App.js
+++ test/src/App.js
@@ -2,6 +2,8 @@ import Navbar from './navbar';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -15,6 +17,12 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
